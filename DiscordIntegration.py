b84eaecc7b382dc7cb39a82a1629cae579874d86
TheApplePieGod <fishplaysgame@gmail.com>
2020-12-13 21:36:20 -0600

    Allow multiple subscribed users

--- DiscordIntegration.py
+++ DiscordIntegration.py
@@ -1,27 +1,41 @@
 import discord
 import asyncio
+import json
 from colorama import Fore
 
 import Logging
 
 client = discord.Client()
-member = None
+members = []
 
 async def init_users():
-    global member
-    infoFile = open("UserInfo.txt", "r")
-    info = infoFile.read()
-    infoFile.close()
-    member = await client.fetch_user(int(info))
+    global members
+    membersFile = open("UserInfo.txt", "r")
+    info = membersFile.read()
+    membersFile.close()
+    memberIds = info.split(',')
+    for id in memberIds:
+        newMember = await client.fetch_user(int(id))
+        members.append(newMember)
     Logging.logWithTimestamp("Discord ready")
 
 async def discord_notify(message):
-    if member != None:
-        channel = await member.create_dm()
-        await channel.send(message)
-        Logging.logWithTimestamp("Discord notified", Fore.YELLOW)
-    else:
-        Logging.logWithTimestamp("Discord notify failed", Fore.RED)
+    success = 0
+    failed = 0
+    for member in members:
+        if member != None:
+            try:
+                channel = await member.create_dm()
+                await channel.send(message)
+                success += 1
+            except:
+                failed += 1
+        else:
+            failed += 1
+    if success > 0:
+        Logging.logWithTimestamp("Discord notified succeeded " + str(success) + '/' + str(len(members)) + " times", Fore.YELLOW)
+    if failed > 0:
+        Logging.logWithTimestamp("Discord notify failed " + str(failed) + '/' + str(len(members)) + " times", Fore.RED)
 
 tokenFile = open("DiscordToken.txt", "r")
 discordBotToken = tokenFile.read()
